ab6b1ab8cea2bc5018d221f0558e70bda3665bb8
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-07-02 16:23:14 -0300

    home screen

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'package:plants/screens/home/widgets/body_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: BodyWidget(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset('assets/icons/menu.svg'),
+        onPressed: () {},
+      ),
+    );
+  }
+}
