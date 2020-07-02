b29e5fdf405ff8c221fd9376c86fa9fcd9f1379f
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-07-02 14:22:52 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/br/martins/plants/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.br.martins.plants
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Page(),
+    );
+  }
+}
+
+class Page extends StatefulWidget {
+  @override
+  _PageState createState() => _PageState();
+}
+
+class _PageState extends State<Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
